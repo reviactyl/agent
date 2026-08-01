chredeur/wings

Improve backup handling

router/router_server_backup.go
@@ -1,18 +1,38 @@
 package router
 
 import (
+	"context"
+	stderrors "errors"
+	"mime"
+	"net"
 	"net/http"
+	"net/netip"
+	"net/url"
 	"os"
 	"strings"
+	"time"
 
 	"emperror.dev/errors"
 	"github.com/apex/log"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 
+	"github.com/pterodactyl/wings/config"
 	"github.com/pterodactyl/wings/router/middleware"
 	"github.com/pterodactyl/wings/server"
 	"github.com/pterodactyl/wings/server/backup"
 )
+
+var blockedBackupRestorePrefixes = []netip.Prefix{
+	netip.MustParsePrefix("100.64.0.0/10"),
+	netip.MustParsePrefix("198.18.0.0/15"),
+}
+
+type backupDownloadError string
+
+func (e backupDownloadError) Error() string {
+	return string(e)
+}
 
 // postServerBackup performs a backup against a given server instance using the
 // provided backup adapter.
@@ -28,13 +48,17 @@ func postServerBackup(c *gin.Context) {
 	if err := c.BindJSON(&data); err != nil {
 		return
 	}
+	backupUuid, ok := parseBackupUuid(c, data.Uuid)
+	if !ok {
+		return
+	}
 
 	var adapter backup.BackupInterface
 	switch data.Adapter {
 	case backup.LocalBackupAdapter:
-		adapter = backup.NewLocal(client, data.Uuid, data.Ignore)
+		adapter = backup.NewLocal(client, backupUuid, data.Ignore)
 	case backup.S3BackupAdapter:
-		adapter = backup.NewS3(client, data.Uuid, data.Ignore)
+		adapter = backup.NewS3(client, backupUuid, data.Ignore)
 	default:
 		middleware.CaptureAndAbort(c, errors.New("router/backups: provided adapter is not valid: "+string(data.Adapter)))
 		return
@@ -80,9 +104,19 @@ func postServerRestoreBackup(c *gin.Context) {
 	if err := c.BindJSON(&data); err != nil {
 		return
 	}
+	backupUuid, ok := parseBackupUuid(c, c.Param("backup"))
+	if !ok {
+		return
+	}
 	if data.Adapter == backup.S3BackupAdapter && data.DownloadUrl == "" {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "The download_url field is required when the backup adapter is set to S3."})
 		return
+	}
+	if data.Adapter == backup.S3BackupAdapter {
+		if err := validateBackupDownloadUrl(data.DownloadUrl); err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
 	}
 
 	s.SetRestoring(true)
@@ -107,7 +141,7 @@ func postServerRestoreBackup(c *gin.Context) {
 	// Now that we've cleaned up the data directory if necessary, grab the backup file
 	// and attempt to restore it into the server directory.
 	if data.Adapter == backup.LocalBackupAdapter {
-		b, _, err := backup.LocateLocal(client, c.Param("backup"))
+		b, _, err := backup.LocateLocal(client, backupUuid)
 		if err != nil {
 			middleware.CaptureAndAbort(c, err)
 			return
@@ -129,7 +163,7 @@ func postServerRestoreBackup(c *gin.Context) {
 
 	// Since this is not a local backup we need to stream the archive and then
 	// parse over the contents as we go in order to restore it to the server.
-	httpClient := http.Client{}
+	httpClient := backupRestoreHttpClient()
 	logger.Info("downloading backup from remote location...")
 	// TODO: this will hang if there is an issue. We can't use c.Request.Context() (or really any)
 	//  since it will be canceled when the request is closed which happens quickly since we push
@@ -144,11 +178,21 @@ func postServerRestoreBackup(c *gin.Context) {
 	}
 	res, err := httpClient.Do(req)
 	if err != nil {
+		var downloadErr backupDownloadError
+		if stderrors.As(err, &downloadErr) {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": downloadErr.Error()})
+			return
+		}
 		middleware.CaptureAndAbort(c, err)
 		return
 	}
+	if res.StatusCode != http.StatusOK {
+		_ = res.Body.Close()
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "The provided backup link returned an invalid response status: " + res.Status})
+		return
+	}
 	// Don't allow content types that we know are going to give us problems.
-	if res.Header.Get("Content-Type") == "" || !strings.Contains("application/x-gzip application/gzip", res.Header.Get("Content-Type")) {
+	if !isSupportedBackupRestoreContentType(res.Header.Get("Content-Type")) {
 		_ = res.Body.Close()
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
 			"error": "The provided backup link is not a supported content type. \"" + res.Header.Get("Content-Type") + "\" is not application/x-gzip.",
@@ -165,7 +209,7 @@ func postServerRestoreBackup(c *gin.Context) {
 		s.Events().Publish(server.BackupRestoreCompletedEvent, "")
 		logger.Info("completed server restoration from S3 backup")
 		s.SetRestoring(false)
-	}(s, c.Param("backup"), logger)
+	}(s, backupUuid, logger)
 
 	hasError = false
 	c.Status(http.StatusAccepted)
@@ -176,7 +220,11 @@ func postServerRestoreBackup(c *gin.Context) {
 // endpoint can make its own decisions as to how it wants to handle that
 // response.
 func deleteServerBackup(c *gin.Context) {
-	b, _, err := backup.LocateLocal(middleware.ExtractApiClient(c), c.Param("backup"))
+	backupUuid, ok := parseBackupUuid(c, c.Param("backup"))
+	if !ok {
+		return
+	}
+	b, _, err := backup.LocateLocal(middleware.ExtractApiClient(c), backupUuid)
 	if err != nil {
 		// Just return from the function at this point if the backup was not located.
 		if errors.Is(err, os.ErrNotExist) {
@@ -196,4 +244,127 @@ func deleteServerBackup(c *gin.Context) {
 		return
 	}
 	c.Status(http.StatusNoContent)
+}
+
+func parseBackupUuid(c *gin.Context, value string) (string, bool) {
+	parsed, err := uuid.Parse(value)
+	if err == nil && len(value) == len(parsed.String()) && parsed.String() == strings.ToLower(value) {
+		return parsed.String(), true
+	}
+	c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "The backup identifier must be a valid UUID."})
+	return "", false
+}
+
+func validateBackupDownloadUrl(raw string) error {
+	parsed, err := url.Parse(raw)
+	if err != nil || parsed.Host == "" {
+		return backupDownloadError("The provided backup link is not a valid URL.")
+	}
+	if parsed.Scheme != "http" && parsed.Scheme != "https" {
+		return backupDownloadError("The provided backup link must use HTTP or HTTPS.")
+	}
+	if ip := net.ParseIP(parsed.Hostname()); ip != nil && isBlockedBackupRestoreIP(parsed.Hostname(), ip) {
+		return backupDownloadError("The provided backup link resolves to a blocked address.")
+	}
+	return nil
+}
+
+func backupRestoreHttpClient() http.Client {
+	transport := http.DefaultTransport.(*http.Transport).Clone()
+	transport.Proxy = nil
+	transport.ResponseHeaderTimeout = 30 * time.Second
+	dialer := &net.Dialer{
+		Timeout:   30 * time.Second,
+		KeepAlive: 30 * time.Second,
+	}
+	transport.DialContext = func(ctx context.Context, network string, address string) (net.Conn, error) {
+		host, port, err := net.SplitHostPort(address)
+		if err != nil {
+			return nil, err
+		}
+		ips, err := net.DefaultResolver.LookupIPAddr(ctx, host)
+		if err != nil {
+			return nil, err
+		}
+		if len(ips) == 0 {
+			return nil, errors.New("router/backups: backup download host did not resolve to any addresses")
+		}
+		for _, resolved := range ips {
+			if isBlockedBackupRestoreIP(host, resolved.IP) {
+				return nil, backupDownloadError("The provided backup link resolves to a blocked address.")
+			}
+		}
+		var lastErr error
+		for _, resolved := range ips {
+			conn, err := dialer.DialContext(ctx, network, net.JoinHostPort(resolved.IP.String(), port))
+			if err == nil {
+				return conn, nil
+			}
+			lastErr = err
+		}
+		return nil, lastErr
+	}
+	return http.Client{
+		Transport: transport,
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			if len(via) >= 10 {
+				return backupDownloadError("The provided backup link redirects too many times.")
+			}
+			return validateBackupDownloadUrl(req.URL.String())
+		},
+	}
+}
+
+func isBlockedBackupRestoreIP(host string, ip net.IP) bool {
+	addr, ok := netip.AddrFromSlice(ip)
+	if !ok {
+		return true
+	}
+	addr = addr.Unmap()
+	if !addr.IsGlobalUnicast() || addr.IsPrivate() || addr.IsLoopback() || addr.IsLinkLocalUnicast() || isExplicitlyBlockedBackupRestoreIP(addr) {
+		return !isAllowedBackupRestoreDestination(host, addr)
+	}
+	return false
+}
+
+func isExplicitlyBlockedBackupRestoreIP(addr netip.Addr) bool {
+	for _, prefix := range blockedBackupRestorePrefixes {
+		if prefix.Contains(addr) {
+			return true
+		}
+	}
+	return false
+}
+
+func isAllowedBackupRestoreDestination(host string, addr netip.Addr) bool {
+	host = strings.TrimSuffix(strings.ToLower(host), ".")
+	for _, entry := range config.Get().System.Backups.RestoreHostAllowlist {
+		entry = strings.TrimSuffix(strings.ToLower(strings.TrimSpace(entry)), ".")
+		if entry == "" {
+			continue
+		}
+		if entry == host {
+			return true
+		}
+		if allowedAddr, err := netip.ParseAddr(entry); err == nil && allowedAddr.Unmap() == addr {
+			return true
+		}
+		if prefix, err := netip.ParsePrefix(entry); err == nil && prefix.Contains(addr) {
+			return true
+		}
+	}
+	return false
+}
+
+func isSupportedBackupRestoreContentType(value string) bool {
+	mediaType, _, err := mime.ParseMediaType(value)
+	if err != nil {
+		mediaType = strings.TrimSpace(value)
+	}
+	switch strings.ToLower(mediaType) {
+	case "application/x-gzip", "application/gzip":
+		return true
+	default:
+		return false
+	}
 }

reviactyl/agent

router/router_server_backup.go
@@ -1,18 +1,38 @@
 package router
 
 import (
+	"context"
+	stderrors "errors"
+	"mime"
+	"net"
 	"net/http"
+	"net/netip"
+	"net/url"
 	"os"
 	"strings"
+	"time"
 
 	"emperror.dev/errors"
 	"github.com/apex/log"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 
+	"github.com/reviactyl/agent/config"
 	"github.com/reviactyl/agent/router/middleware"
 	"github.com/reviactyl/agent/server"
 	"github.com/reviactyl/agent/server/backup"
 )
+
+var blockedBackupRestorePrefixes = []netip.Prefix{
+	netip.MustParsePrefix("100.64.0.0/10"),
+	netip.MustParsePrefix("198.18.0.0/15"),
+}
+
+type backupDownloadError string
+
+func (e backupDownloadError) Error() string {
+	return string(e)
+}
 
 // postServerBackup performs a backup against a given server instance using the
 // provided backup adapter.
@@ -28,13 +48,17 @@ func postServerBackup(c *gin.Context) {
 	if err := c.BindJSON(&data); err != nil {
 		return
 	}
+	backupUuid, ok := parseBackupUuid(c, data.Uuid)
+	if !ok {
+		return
+	}
 
 	var adapter backup.BackupInterface
 	switch data.Adapter {
 	case backup.LocalBackupAdapter:
-		adapter = backup.NewLocal(client, data.Uuid, data.Ignore)
+		adapter = backup.NewLocal(client, backupUuid, data.Ignore)
 	case backup.S3BackupAdapter:
-		adapter = backup.NewS3(client, data.Uuid, data.Ignore)
+		adapter = backup.NewS3(client, backupUuid, data.Ignore)
 	default:
 		middleware.CaptureAndAbort(c, errors.New("router/backups: provided adapter is not valid: "+string(data.Adapter)))
 		return
@@ -80,9 +104,19 @@ func postServerRestoreBackup(c *gin.Context) {
 	if err := c.BindJSON(&data); err != nil {
 		return
 	}
+	backupUuid, ok := parseBackupUuid(c, c.Param("backup"))
+	if !ok {
+		return
+	}
 	if data.Adapter == backup.S3BackupAdapter && data.DownloadUrl == "" {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "The download_url field is required when the backup adapter is set to S3."})
 		return
+	}
+	if data.Adapter == backup.S3BackupAdapter {
+		if err := validateBackupDownloadUrl(data.DownloadUrl); err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
 	}
 
 	s.SetRestoring(true)
@@ -107,7 +141,7 @@ func postServerRestoreBackup(c *gin.Context) {
 	// Now that we've cleaned up the data directory if necessary, grab the backup file
 	// and attempt to restore it into the server directory.
 	if data.Adapter == backup.LocalBackupAdapter {
-		b, _, err := backup.LocateLocal(client, c.Param("backup"))
+		b, _, err := backup.LocateLocal(client, backupUuid)
 		if err != nil {
 			middleware.CaptureAndAbort(c, err)
 			return
@@ -129,7 +163,7 @@ func postServerRestoreBackup(c *gin.Context) {
 
 	// Since this is not a local backup we need to stream the archive and then
 	// parse over the contents as we go in order to restore it to the server.
-	httpClient := http.Client{}
+	httpClient := backupRestoreHttpClient()
 	logger.Info("downloading backup from remote location...")
 	// TODO: this will hang if there is an issue. We can't use c.Request.Context() (or really any)
 	//  since it will be canceled when the request is closed which happens quickly since we push
@@ -144,11 +178,21 @@ func postServerRestoreBackup(c *gin.Context) {
 	}
 	res, err := httpClient.Do(req)
 	if err != nil {
+		var downloadErr backupDownloadError
+		if stderrors.As(err, &downloadErr) {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": downloadErr.Error()})
+			return
+		}
 		middleware.CaptureAndAbort(c, err)
 		return
 	}
+	if res.StatusCode != http.StatusOK {
+		_ = res.Body.Close()
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "The provided backup link returned an invalid response status: " + res.Status})
+		return
+	}
 	// Don't allow content types that we know are going to give us problems.
-	if res.Header.Get("Content-Type") == "" || !strings.Contains("application/x-gzip application/gzip", res.Header.Get("Content-Type")) {
+	if !isSupportedBackupRestoreContentType(res.Header.Get("Content-Type")) {
 		_ = res.Body.Close()
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
 			"error": "The provided backup link is not a supported content type. \"" + res.Header.Get("Content-Type") + "\" is not application/x-gzip.",
@@ -165,7 +209,7 @@ func postServerRestoreBackup(c *gin.Context) {
 		s.Events().Publish(server.BackupRestoreCompletedEvent, "")
 		logger.Info("completed server restoration from S3 backup")
 		s.SetRestoring(false)
-	}(s, c.Param("backup"), logger)
+	}(s, backupUuid, logger)
 
 	hasError = false
 	c.Status(http.StatusAccepted)
@@ -176,7 +220,11 @@ func postServerRestoreBackup(c *gin.Context) {
 // endpoint can make its own decisions as to how it wants to handle that
 // response.
 func deleteServerBackup(c *gin.Context) {
-	b, _, err := backup.LocateLocal(middleware.ExtractApiClient(c), c.Param("backup"))
+	backupUuid, ok := parseBackupUuid(c, c.Param("backup"))
+	if !ok {
+		return
+	}
+	b, _, err := backup.LocateLocal(middleware.ExtractApiClient(c), backupUuid)
 	if err != nil {
 		// Just return from the function at this point if the backup was not located.
 		if errors.Is(err, os.ErrNotExist) {
@@ -196,4 +244,127 @@ func deleteServerBackup(c *gin.Context) {
 		return
 	}
 	c.Status(http.StatusNoContent)
+}
+
+func parseBackupUuid(c *gin.Context, value string) (string, bool) {
+	parsed, err := uuid.Parse(value)
+	if err == nil && len(value) == len(parsed.String()) && parsed.String() == strings.ToLower(value) {
+		return parsed.String(), true
+	}
+	c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "The backup identifier must be a valid UUID."})
+	return "", false
+}
+
+func validateBackupDownloadUrl(raw string) error {
+	parsed, err := url.Parse(raw)
+	if err != nil || parsed.Host == "" {
+		return backupDownloadError("The provided backup link is not a valid URL.")
+	}
+	if parsed.Scheme != "http" && parsed.Scheme != "https" {
+		return backupDownloadError("The provided backup link must use HTTP or HTTPS.")
+	}
+	if ip := net.ParseIP(parsed.Hostname()); ip != nil && isBlockedBackupRestoreIP(parsed.Hostname(), ip) {
+		return backupDownloadError("The provided backup link resolves to a blocked address.")
+	}
+	return nil
+}
+
+func backupRestoreHttpClient() http.Client {
+	transport := http.DefaultTransport.(*http.Transport).Clone()
+	transport.Proxy = nil
+	transport.ResponseHeaderTimeout = 30 * time.Second
+	dialer := &net.Dialer{
+		Timeout:   30 * time.Second,
+		KeepAlive: 30 * time.Second,
+	}
+	transport.DialContext = func(ctx context.Context, network string, address string) (net.Conn, error) {
+		host, port, err := net.SplitHostPort(address)
+		if err != nil {
+			return nil, err
+		}
+		ips, err := net.DefaultResolver.LookupIPAddr(ctx, host)
+		if err != nil {
+			return nil, err
+		}
+		if len(ips) == 0 {
+			return nil, errors.New("router/backups: backup download host did not resolve to any addresses")
+		}
+		for _, resolved := range ips {
+			if isBlockedBackupRestoreIP(host, resolved.IP) {
+				return nil, backupDownloadError("The provided backup link resolves to a blocked address.")
+			}
+		}
+		var lastErr error
+		for _, resolved := range ips {
+			conn, err := dialer.DialContext(ctx, network, net.JoinHostPort(resolved.IP.String(), port))
+			if err == nil {
+				return conn, nil
+			}
+			lastErr = err
+		}
+		return nil, lastErr
+	}
+	return http.Client{
+		Transport: transport,
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			if len(via) >= 10 {
+				return backupDownloadError("The provided backup link redirects too many times.")
+			}
+			return validateBackupDownloadUrl(req.URL.String())
+		},
+	}
+}
+
+func isBlockedBackupRestoreIP(host string, ip net.IP) bool {
+	addr, ok := netip.AddrFromSlice(ip)
+	if !ok {
+		return true
+	}
+	addr = addr.Unmap()
+	if !addr.IsGlobalUnicast() || addr.IsPrivate() || addr.IsLoopback() || addr.IsLinkLocalUnicast() || isExplicitlyBlockedBackupRestoreIP(addr) {
+		return !isAllowedBackupRestoreDestination(host, addr)
+	}
+	return false
+}
+
+func isExplicitlyBlockedBackupRestoreIP(addr netip.Addr) bool {
+	for _, prefix := range blockedBackupRestorePrefixes {
+		if prefix.Contains(addr) {
+			return true
+		}
+	}
+	return false
+}
+
+func isAllowedBackupRestoreDestination(host string, addr netip.Addr) bool {
+	host = strings.TrimSuffix(strings.ToLower(host), ".")
+	for _, entry := range config.Get().System.Backups.RestoreHostAllowlist {
+		entry = strings.TrimSuffix(strings.ToLower(strings.TrimSpace(entry)), ".")
+		if entry == "" {
+			continue
+		}
+		if entry == host {
+			return true
+		}
+		if allowedAddr, err := netip.ParseAddr(entry); err == nil && allowedAddr.Unmap() == addr {
+			return true
+		}
+		if prefix, err := netip.ParsePrefix(entry); err == nil && prefix.Contains(addr) {
+			return true
+		}
+	}
+	return false
+}
+
+func isSupportedBackupRestoreContentType(value string) bool {
+	mediaType, _, err := mime.ParseMediaType(value)
+	if err != nil {
+		mediaType = strings.TrimSpace(value)
+	}
+	switch strings.ToLower(mediaType) {
+	case "application/x-gzip", "application/gzip":
+		return true
+	default:
+		return false
+	}
 }