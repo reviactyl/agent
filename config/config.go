SimpleGameHosting/wings

Harden credential rotation

config/config.go
@@ -417,11 +417,22 @@ func Set(c *Configuration) {
 // Set remote when the values came from the Panel. Local values may use
 // "file://" or "$VAR" indirection; expanding one sent over the network would
 // leak files and environment variables back out through the token we attach to
-// every request.
+// every request. Environment overrides must already match remote values so a
+// configuration update cannot leave Wings and the Panel using different keys.
 func (c *Configuration) ResolveToken(remote bool) error {
-	resolve := func(env, local string) (string, error) {
+	resolve := func(name, env, local string) (string, error) {
+		if remote && (strings.Contains(local, "$") || strings.HasPrefix(local, "file://")) {
+			return "", fmt.Errorf("config: remote %s cannot use token indirection", name)
+		}
 		if env != "" {
-			return Expand(env)
+			value, err := Expand(env)
+			if err != nil {
+				return "", err
+			}
+			if remote && value != local {
+				return "", fmt.Errorf("config: remote %s does not match environment override", name)
+			}
+			return value, nil
 		}
 		if remote {
 			return local, nil
@@ -430,10 +441,10 @@ func (c *Configuration) ResolveToken(remote bool) error {
 	}
 
 	var err error
-	if c.Token.ID, err = resolve(os.Getenv("WINGS_TOKEN_ID"), c.AuthenticationTokenId); err != nil {
+	if c.Token.ID, err = resolve("token ID", os.Getenv("WINGS_TOKEN_ID"), c.AuthenticationTokenId); err != nil {
 		return err
 	}
-	if c.Token.Token, err = resolve(os.Getenv("WINGS_TOKEN"), c.AuthenticationToken); err != nil {
+	if c.Token.Token, err = resolve("token", os.Getenv("WINGS_TOKEN"), c.AuthenticationToken); err != nil {
 		return err
 	}
 	return nil

config/config_token_test.go
@@ -1,0 +1,60 @@
+package config
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestResolveRemoteToken(t *testing.T) {
+	t.Setenv("WINGS_TOKEN_ID", "")
+	t.Setenv("WINGS_TOKEN", "")
+
+	cfg := Configuration{
+		AuthenticationTokenId: "panel-id",
+		AuthenticationToken:   "panel-token",
+	}
+	if err := cfg.ResolveToken(true); err != nil {
+		t.Fatalf("expected remote credentials to resolve: %v", err)
+	}
+	if cfg.Token.ID != "panel-id" || cfg.Token.Token != "panel-token" {
+		t.Fatalf("unexpected resolved credentials: %#v", cfg.Token)
+	}
+}
+
+func TestResolveRemoteTokenRejectsIndirection(t *testing.T) {
+	t.Setenv("WINGS_TOKEN_ID", "")
+	t.Setenv("WINGS_TOKEN", "")
+
+	tests := []Configuration{
+		{AuthenticationTokenId: "file:///tmp/id", AuthenticationToken: "panel-token"},
+		{AuthenticationTokenId: "panel-id", AuthenticationToken: "$PANEL_TOKEN"},
+	}
+	for _, cfg := range tests {
+		if err := cfg.ResolveToken(true); err == nil {
+			t.Fatal("expected remote token indirection to be rejected")
+		}
+	}
+}
+
+func TestResolveRemoteTokenRequiresEnvironmentMatch(t *testing.T) {
+	secret := filepath.Join(t.TempDir(), "token")
+	if err := os.WriteFile(secret, []byte("panel-token\n"), 0o600); err != nil {
+		t.Fatal(err)
+	}
+	t.Setenv("WINGS_TOKEN_ID", "panel-id")
+	t.Setenv("WINGS_TOKEN", "file://"+secret)
+
+	cfg := Configuration{
+		AuthenticationTokenId: "panel-id",
+		AuthenticationToken:   "panel-token",
+	}
+	if err := cfg.ResolveToken(true); err != nil {
+		t.Fatalf("expected matching environment credentials to resolve: %v", err)
+	}
+
+	cfg.AuthenticationToken = "rotated-token"
+	if err := cfg.ResolveToken(true); err == nil {
+		t.Fatal("expected mismatched environment credentials to be rejected")
+	}
+}

router/router_system_test.go
@@ -1,0 +1,55 @@
+package router
+
+import (
+	"net/http/httptest"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/pterodactyl/wings/config"
+	"github.com/pterodactyl/wings/server"
+)
+
+func TestPostUpdateConfigurationRotatesCredentials(t *testing.T) {
+	t.Setenv("WINGS_TOKEN_ID", "")
+	t.Setenv("WINGS_TOKEN", "")
+
+	cfg, err := config.NewAtPath(filepath.Join(t.TempDir(), "config.yml"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	cfg.AuthenticationTokenId = "old-id"
+	cfg.AuthenticationToken = "old-token"
+	if err := cfg.ResolveToken(false); err != nil {
+		t.Fatal(err)
+	}
+	config.Set(cfg)
+
+	credentials := make(chan [2]string, 1)
+	manager := server.NewEmptyManager(backupTestRemoteClient{credentials: credentials})
+	recorder := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(recorder)
+	c.Set("manager", manager)
+	c.Request = httptest.NewRequest("POST", "/api/update", strings.NewReader(`{"token_id":"new-id","token":"new-token"}`))
+	c.Request.Header.Set("Content-Type", "application/json")
+
+	postUpdateConfiguration(c)
+
+	if recorder.Code != 200 {
+		t.Fatalf("expected successful update, got status %d", recorder.Code)
+	}
+	updated := config.Get()
+	if updated.Token.ID != "new-id" || updated.Token.Token != "new-token" {
+		t.Fatalf("unexpected resolved credentials: %#v", updated.Token)
+	}
+	select {
+	case got := <-credentials:
+		if got != [2]string{"new-id", "new-token"} {
+			t.Fatalf("unexpected client credentials: %#v", got)
+		}
+	default:
+		t.Fatal("expected client credentials to be rotated")
+	}
+}

reviactyl/agent

config/config.go
@@ -417,11 +417,22 @@ func Set(c *Configuration) {
 // Set remote when the values came from the Panel. Local values may use
 // "file://" or "$VAR" indirection; expanding one sent over the network would
 // leak files and environment variables back out through the token we attach to
-// every request.
+// every request. Environment overrides must already match remote values so a
+// configuration update cannot leave Agent and the Panel using different keys.
 func (c *Configuration) ResolveToken(remote bool) error {
-	resolve := func(env, local string) (string, error) {
+	resolve := func(name, env, local string) (string, error) {
+		if remote && (strings.Contains(local, "$") || strings.HasPrefix(local, "file://")) {
+			return "", fmt.Errorf("config: remote %s cannot use token indirection", name)
+		}
 		if env != "" {
-			return Expand(env)
+			value, err := Expand(env)
+			if err != nil {
+				return "", err
+			}
+			if remote && value != local {
+				return "", fmt.Errorf("config: remote %s does not match environment override", name)
+			}
+			return value, nil
 		}
 		if remote {
 			return local, nil
@@ -430,10 +441,10 @@ func (c *Configuration) ResolveToken(remote bool) error {
 	}
 
 	var err error
-	if c.Token.ID, err = resolve(os.Getenv("AGENT_TOKEN_ID"), c.AuthenticationTokenId); err != nil {
+	if c.Token.ID, err = resolve("token ID", os.Getenv("AGENT_TOKEN_ID"), c.AuthenticationTokenId); err != nil {
 		return err
 	}
-	if c.Token.Token, err = resolve(os.Getenv("AGENT_TOKEN"), c.AuthenticationToken); err != nil {
+	if c.Token.Token, err = resolve("token", os.Getenv("AGENT_TOKEN"), c.AuthenticationToken); err != nil {
 		return err
 	}
 	return nil

config/config_token_test.go
@@ -1,0 +1,60 @@
+package config
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestResolveRemoteToken(t *testing.T) {
+	t.Setenv("AGENT_TOKEN_ID", "")
+	t.Setenv("AGENT_TOKEN", "")
+
+	cfg := Configuration{
+		AuthenticationTokenId: "panel-id",
+		AuthenticationToken:   "panel-token",
+	}
+	if err := cfg.ResolveToken(true); err != nil {
+		t.Fatalf("expected remote credentials to resolve: %v", err)
+	}
+	if cfg.Token.ID != "panel-id" || cfg.Token.Token != "panel-token" {
+		t.Fatalf("unexpected resolved credentials: %#v", cfg.Token)
+	}
+}
+
+func TestResolveRemoteTokenRejectsIndirection(t *testing.T) {
+	t.Setenv("AGENT_TOKEN_ID", "")
+	t.Setenv("AGENT_TOKEN", "")
+
+	tests := []Configuration{
+		{AuthenticationTokenId: "file:///tmp/id", AuthenticationToken: "panel-token"},
+		{AuthenticationTokenId: "panel-id", AuthenticationToken: "$PANEL_TOKEN"},
+	}
+	for _, cfg := range tests {
+		if err := cfg.ResolveToken(true); err == nil {
+			t.Fatal("expected remote token indirection to be rejected")
+		}
+	}
+}
+
+func TestResolveRemoteTokenRequiresEnvironmentMatch(t *testing.T) {
+	secret := filepath.Join(t.TempDir(), "token")
+	if err := os.WriteFile(secret, []byte("panel-token\n"), 0o600); err != nil {
+		t.Fatal(err)
+	}
+	t.Setenv("AGENT_TOKEN_ID", "panel-id")
+	t.Setenv("AGENT_TOKEN", "file://"+secret)
+
+	cfg := Configuration{
+		AuthenticationTokenId: "panel-id",
+		AuthenticationToken:   "panel-token",
+	}
+	if err := cfg.ResolveToken(true); err != nil {
+		t.Fatalf("expected matching environment credentials to resolve: %v", err)
+	}
+
+	cfg.AuthenticationToken = "rotated-token"
+	if err := cfg.ResolveToken(true); err == nil {
+		t.Fatal("expected mismatched environment credentials to be rejected")
+	}
+}

router/router_system_test.go
@@ -1,0 +1,55 @@
+package router
+
+import (
+	"net/http/httptest"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/reviactyl/agent/config"
+	"github.com/reviactyl/agent/server"
+)
+
+func TestPostUpdateConfigurationRotatesCredentials(t *testing.T) {
+	t.Setenv("AGENT_TOKEN_ID", "")
+	t.Setenv("AGENT_TOKEN", "")
+
+	cfg, err := config.NewAtPath(filepath.Join(t.TempDir(), "config.yml"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	cfg.AuthenticationTokenId = "old-id"
+	cfg.AuthenticationToken = "old-token"
+	if err := cfg.ResolveToken(false); err != nil {
+		t.Fatal(err)
+	}
+	config.Set(cfg)
+
+	credentials := make(chan [2]string, 1)
+	manager := server.NewEmptyManager(backupTestRemoteClient{credentials: credentials})
+	recorder := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(recorder)
+	c.Set("manager", manager)
+	c.Request = httptest.NewRequest("POST", "/api/update", strings.NewReader(`{"token_id":"new-id","token":"new-token"}`))
+	c.Request.Header.Set("Content-Type", "application/json")
+
+	postUpdateConfiguration(c)
+
+	if recorder.Code != 200 {
+		t.Fatalf("expected successful update, got status %d", recorder.Code)
+	}
+	updated := config.Get()
+	if updated.Token.ID != "new-id" || updated.Token.Token != "new-token" {
+		t.Fatalf("unexpected resolved credentials: %#v", updated.Token)
+	}
+	select {
+	case got := <-credentials:
+		if got != [2]string{"new-id", "new-token"} {
+			t.Fatalf("unexpected client credentials: %#v", got)
+		}
+	default:
+		t.Fatal("expected client credentials to be rotated")
+	}
+}