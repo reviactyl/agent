SimpleGameHosting/wings

fix: master key resets through the panel now automatically propogate on the daemon

config/config.go
@@ -411,6 +411,41 @@ func Set(c *Configuration) {
 	_config = c
 }
 
+// ResolveRemoteToken populates the derived Token field after the Panel has sent
+// us new token values. Because the resolved token is what everything else in
+// Wings authenticates against, this has to be called whenever the underlying
+// AuthenticationToken values change, otherwise the previously resolved token
+// stays in use until the process is restarted.
+func (c *Configuration) ResolveRemoteToken() error {
+	return c.resolveToken(false)
+}
+
+// resolveToken resolves the token to use, preferring values pinned through the
+// environment so that a token supplied by the system running Wings is never
+// replaced by one sent to us by the Panel. expandLocal controls whether the
+// values held in the configuration itself are trusted enough to be passed through
+// Expand.
+func (c *Configuration) resolveToken(expandLocal bool) error {
+	resolve := func(env, local string) (string, error) {
+		if env != "" {
+			return Expand(env)
+		}
+		if expandLocal {
+			return Expand(local)
+		}
+		return local, nil
+	}
+
+	var err error
+	if c.Token.ID, err = resolve(os.Getenv("WINGS_TOKEN_ID"), c.AuthenticationTokenId); err != nil {
+		return err
+	}
+	if c.Token.Token, err = resolve(os.Getenv("WINGS_TOKEN"), c.AuthenticationToken); err != nil {
+		return err
+	}
+	return nil
+}
+
 // SetDebugViaFlag tracks if the application is running in debug mode because of
 // a command line flag argument. If so we do not want to store that configuration
 // change to the disk.
@@ -600,23 +635,7 @@ func FromFile(path string) error {
 		return err
 	}
 
-	c.Token = Token{
-		ID:    os.Getenv("WINGS_TOKEN_ID"),
-		Token: os.Getenv("WINGS_TOKEN"),
-	}
-	if c.Token.ID == "" {
-		c.Token.ID = c.AuthenticationTokenId
-	}
-	if c.Token.Token == "" {
-		c.Token.Token = c.AuthenticationToken
-	}
-
-	c.Token.ID, err = Expand(c.Token.ID)
-	if err != nil {
-		return err
-	}
-	c.Token.Token, err = Expand(c.Token.Token)
-	if err != nil {
+	if err := c.resolveToken(true); err != nil {
 		return err
 	}
 
@@ -860,7 +879,7 @@ func Expand(v string) (string, error) {
 
 		b, err := os.ReadFile(p)
 		if err != nil {
-			return "", nil
+			return "", err
 		}
 		v = string(bytes.TrimRight(bytes.TrimRight(b, "\r"), "\n"))
 	}

remote/http.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/pterodactyl/wings/internal/models"
@@ -33,11 +34,13 @@ type Client interface {
 	SetTransferStatus(ctx context.Context, uuid string, successful bool) error
 	ValidateSftpCredentials(ctx context.Context, request SftpAuthRequest) (SftpAuthResponse, error)
 	SendActivityLogs(ctx context.Context, activity []models.Activity) error
+	SetCredentials(id, token string)
 }
 
 type client struct {
 	httpClient  *http.Client
 	baseUrl     string
+	mu          sync.RWMutex
 	tokenId     string
 	token       string
 	maxAttempts int
@@ -66,6 +69,22 @@ func WithCredentials(id, token string) ClientOption {
 		c.tokenId = id
 		c.token = token
 	}
+}
+
+// SetCredentials replaces the credentials used when making requests to the
+// remote API endpoint.
+func (c *client) SetCredentials(id, token string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.tokenId = id
+	c.token = token
+}
+
+// credentials returns the credentials currently in use by this client.
+func (c *client) credentials() (string, string) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	return c.tokenId, c.token
 }
 
 // WithHttpClient sets the underlying HTTP client instance to use when making
@@ -105,10 +124,11 @@ func (c *client) requestOnce(ctx context.Context, method, path string, body io.R
 		return nil, err
 	}
 
-	req.Header.Set("User-Agent", fmt.Sprintf("Pterodactyl Wings/v%s (id:%s)", system.Version, c.tokenId))
+	tokenId, token := c.credentials()
+	req.Header.Set("User-Agent", fmt.Sprintf("Pterodactyl Wings/v%s (id:%s)", system.Version, tokenId))
 	req.Header.Set("Accept", "application/vnd.pterodactyl.v1+json")
 	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s.%s", c.tokenId, c.token))
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s.%s", tokenId, token))
 
 	// Call all opts functions to allow modifying the request
 	for _, o := range opts {

reviactyl/agent

config/config.go
@@ -411,6 +411,41 @@ func Set(c *Configuration) {
 	_config = c
 }
 
+// ResolveRemoteToken populates the derived Token field after the Panel has sent
+// us new token values. Because the resolved token is what everything else in
+// Agent authenticates against, this has to be called whenever the underlying
+// AuthenticationToken values change, otherwise the previously resolved token
+// stays in use until the process is restarted.
+func (c *Configuration) ResolveRemoteToken() error {
+	return c.resolveToken(false)
+}
+
+// resolveToken resolves the token to use, preferring values pinned through the
+// environment so that a token supplied by the system running Agent is never
+// replaced by one sent to us by the Panel. expandLocal controls whether the
+// values held in the configuration itself are trusted enough to be passed through
+// Expand.
+func (c *Configuration) resolveToken(expandLocal bool) error {
+	resolve := func(env, local string) (string, error) {
+		if env != "" {
+			return Expand(env)
+		}
+		if expandLocal {
+			return Expand(local)
+		}
+		return local, nil
+	}
+
+	var err error
+	if c.Token.ID, err = resolve(os.Getenv("AGENT_TOKEN_ID"), c.AuthenticationTokenId); err != nil {
+		return err
+	}
+	if c.Token.Token, err = resolve(os.Getenv("AGENT_TOKEN"), c.AuthenticationToken); err != nil {
+		return err
+	}
+	return nil
+}
+
 // SetDebugViaFlag tracks if the application is running in debug mode because of
 // a command line flag argument. If so we do not want to store that configuration
 // change to the disk.
@@ -600,23 +635,7 @@ func FromFile(path string) error {
 		return err
 	}
 
-	c.Token = Token{
-		ID:    os.Getenv("AGENT_TOKEN_ID"),
-		Token: os.Getenv("AGENT_TOKEN"),
-	}
-	if c.Token.ID == "" {
-		c.Token.ID = c.AuthenticationTokenId
-	}
-	if c.Token.Token == "" {
-		c.Token.Token = c.AuthenticationToken
-	}
-
-	c.Token.ID, err = Expand(c.Token.ID)
-	if err != nil {
-		return err
-	}
-	c.Token.Token, err = Expand(c.Token.Token)
-	if err != nil {
+	if err := c.resolveToken(true); err != nil {
 		return err
 	}
 
@@ -860,7 +879,7 @@ func Expand(v string) (string, error) {
 
 		b, err := os.ReadFile(p)
 		if err != nil {
-			return "", nil
+			return "", err
 		}
 		v = string(bytes.TrimRight(bytes.TrimRight(b, "\r"), "\n"))
 	}

remote/http.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/reviactyl/agent/internal/models"
@@ -33,11 +34,13 @@ type Client interface {
 	SetTransferStatus(ctx context.Context, uuid string, successful bool) error
 	ValidateSftpCredentials(ctx context.Context, request SftpAuthRequest) (SftpAuthResponse, error)
 	SendActivityLogs(ctx context.Context, activity []models.Activity) error
+	SetCredentials(id, token string)
 }
 
 type client struct {
 	httpClient  *http.Client
 	baseUrl     string
+	mu          sync.RWMutex
 	tokenId     string
 	token       string
 	maxAttempts int
@@ -66,6 +69,22 @@ func WithCredentials(id, token string) ClientOption {
 		c.tokenId = id
 		c.token = token
 	}
+}
+
+// SetCredentials replaces the credentials used when making requests to the
+// remote API endpoint.
+func (c *client) SetCredentials(id, token string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.tokenId = id
+	c.token = token
+}
+
+// credentials returns the credentials currently in use by this client.
+func (c *client) credentials() (string, string) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	return c.tokenId, c.token
 }
 
 // WithHttpClient sets the underlying HTTP client instance to use when making
@@ -105,10 +124,11 @@ func (c *client) requestOnce(ctx context.Context, method, path string, body io.R
 		return nil, err
 	}
 
-	req.Header.Set("User-Agent", fmt.Sprintf("Reviactyl Agent/v%s (id:%s)", system.Version, c.tokenId))
+	tokenId, token := c.credentials()
+	req.Header.Set("User-Agent", fmt.Sprintf("Reviactyl Agent/v%s (id:%s)", system.Version, tokenId))
 	req.Header.Set("Accept", "application/vnd.reviactyl.v26+json")
 	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s.%s", c.tokenId, c.token))
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s.%s", tokenId, token))
 
 	// Call all opts functions to allow modifying the request
 	for _, o := range opts {